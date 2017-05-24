c85c5caad28554456fd922f61248ddcf94c19eed
DESKTOP-BDRN36L\inagaki <norisuna1201@gmail.com>
2017-05-24 15:45:19 +0900

    BaseballModelはHololensでの作動を確認済み BaseballModel/App/BaseballModel.slnからHololensで実行できる

---
+++ BaseballModel/Assets/Scripts/BehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourScript : MonoBehaviour {
+
+    public float Vy,Vz,speed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        Vector3 movement = new Vector3(0.0f, Vy, Vz);
+        rb.AddForce(movement * speed);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z < -5.0f)
+        {
+            print("Space key is held down");
+            rb.velocity = Vector3.zero;
+            transform.position = new Vector3(0f, 0f, 5.0f);
+            Vector3 movement = new Vector3(0.0f, Vy, Vz);
+            rb.AddForce(movement * speed);
+        }
+    }
+}
