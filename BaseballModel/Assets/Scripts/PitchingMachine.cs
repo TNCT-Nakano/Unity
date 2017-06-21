026bc0e7c381fc16dd9476e535016245048eb2ca
norihirosunada <norisuna1201@gmail.com>
2017-06-21 10:46:39 +0900

    Added Pitching Machine

---
+++ BaseballModel/Assets/Scripts/PitchingMachine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchingMachine : MonoBehaviour {
+
+    public GameObject baseball;
+    public Transform target;
+    public float Vy, Vz, speed;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            GameObject ballInstance = Instantiate(baseball, transform.position, transform.rotation);
+            Vector3 movement = new Vector3(0.0f, Vy, Vz);
+            ballInstance.GetComponent<Rigidbody>().AddForce(movement * speed); 
+        }
+	}
+}
