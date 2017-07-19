f0d55c97851810798218d14f7cf32c5a8eb04a48
Getpa <catsinf@gmail.com>
2017-07-19 13:24:12 +0900

    create branch

---
+++ ToolkitTest/Assets/MenuBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour {
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+}
