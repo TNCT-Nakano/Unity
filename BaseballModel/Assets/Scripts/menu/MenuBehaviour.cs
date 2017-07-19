dc35d99be7132ad0b1eed93d5a89101751d9f0ad
norihirosunada <norisuna1201@gmail.com>
2017-07-19 15:40:57 +0900

    メニュー画面との統合　動作確認済み

---
+++ BaseballModel/Assets/Scripts/menu/MenuBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuBehaviour : MonoBehaviour {
+    public GameObject dialog;
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+
+    public void StartSpatialUnderstanding()
+    {
+        GameObject.Find("SpatialUnderstanding").GetComponent<HoloToolkit.Unity.SpatialUnderstanding>().RequestBeginScanning();
+        var msgbox = Instantiate(dialog);
+        msgbox.transform.Find("Panel").Find("Message").gameObject.GetComponent<Text>().text = "starting Spatial Understanding.";
+    }
+
+    public void StopSpatialUnderstanding()
+    {
+        GameObject.Find("SpatialUnderstanding").GetComponent<HoloToolkit.Unity.SpatialUnderstanding>().RequestFinishScan();
+        var msgbox = Instantiate(dialog);
+        msgbox.transform.Find("Panel").Find("Message").gameObject.GetComponent<Text>().text = "stopping Spatial Understanding.";
+    }
+
+    public void BackToTitle()
+    {
+        SceneManager.LoadSceneAsync("StartScene");
+    }
+}
