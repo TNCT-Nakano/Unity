44b05ce200ace6a68bc30c4318ce5d006e9d5235
norihirosunada <norisuna1201@gmail.com>
2017-07-19 13:56:41 +0900

    投球調整用にコンソールに球速の表示と、バットの位置初期化案１の関数だけ実装

--- BaseballModel/Assets/Scripts/ballScript.cs
+++ BaseballModel/Assets/Scripts/ballScript.cs
@@ -6,14 +6,20 @@ using System;
 
 public class ballScript : MonoBehaviour{
     // Use this for initialization
-    void Start () {
 
+    private Rigidbody rb;
+
+    void Start () {
+        rb = this.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //下に10mの地点まで行くと破壊
         if (transform.position.y < -10f) Destroy(gameObject);
-        
+
+        //速度表示
+        Debug.Log("速度：" + rb.velocity.magnitude + "m/s , " + rb.velocity.magnitude/1000*60*60 + "km/h , ");
 	}
 
     //衝突時の処理
