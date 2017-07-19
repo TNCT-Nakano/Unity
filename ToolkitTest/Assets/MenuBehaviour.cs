d3723fb4c7416eda0ead2261dd17fe7db703aff9
Getpa <catsinf@gmail.com>
2017-07-19 14:22:29 +0900

    デバッグ用シーンとその遷移用オブジェクトを配置 デバッグシーンでSpatialUnderstandingを開始できるようにパネルを配置

--- ToolkitTest/Assets/MenuBehaviour.cs
+++ ToolkitTest/Assets/MenuBehaviour.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuBehaviour : MonoBehaviour {
-
+    public GameObject dialog;
     public void Destroy()
     {
         Destroy(gameObject);
@@ -12,5 +14,24 @@ public class MenuBehaviour : MonoBehaviour {
     private void Start()
     {
         GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+
+    public void StartSpatialUnderstanding()
+    {
+        GameObject.Find("SpatialUnderstanding").GetComponent<HoloToolkit.Unity.SpatialUnderstanding>().RequestBeginScanning();
+        var msgbox = Instantiate(dialog);
+        msgbox.transform.Find("Panel").Find("Message").gameObject.GetComponent<Text>().text = "starting Spatial Understanding.";
+    }
+
+    public void StopSpatialUnderstanding()
+    {
+        GameObject.Find("SpatialUnderstanding").GetComponent<HoloToolkit.Unity.SpatialUnderstanding>().RequestFinishScan();
+        var msgbox = Instantiate(dialog);
+        msgbox.transform.Find("Panel").Find("Message").gameObject.GetComponent<Text>().text = "stopping Spatial Understanding.";
+    }
+
+    public void BackToTitle()
+    {
+        SceneManager.LoadSceneAsync("StartScene");
     }
 }
---
+++ ToolkitTest/Assets/infoBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infoBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(autoDestroy());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator autoDestroy()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+}
