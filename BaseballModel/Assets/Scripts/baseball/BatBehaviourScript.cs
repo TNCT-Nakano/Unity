7778ed3ad007e807a098310539f35ae36017be74
getpa <catsinf@gmail.com>
2017-08-09 15:22:09 +0900

    バット初期化のためのボイスコマンドとホログラムを実装

--- BaseballModel/Assets/Scripts/baseball/BatBehaviourScript.cs
+++ BaseballModel/Assets/Scripts/baseball/BatBehaviourScript.cs
@@ -1,77 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using HoloToolkit.Unity.InputModule;
 
-public class BatBehaviourScript : MonoBehaviour, INavigationHandler {
+public class BatBehaviourScript : MonoBehaviour{
 
     private float x, y, z;
-    private Rigidbody rigidbody;
+    private BlueComm bc;
 
     // Use this for initialization
     void Start () {
         //rigidbody.centerOfMass = new Vector3(0, 0, 1);
 
-        BlueComm bc = GetComponent<BlueComm>();
-
-        //位置初期化
-        //BatInit();
+        bc = GetComponent<BlueComm>();
     }
 
-    //バットの位置初期化
-    void BatInit()
-    {
-        transform.position = new Vector3(0, 0, transform.localScale.y);
-        transform.rotation = Quaternion.Euler(90, 0, 0);
-    }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
         //マウスの位置によって回転させる
         Vector3 pos = Input.mousePosition;
         transform.rotation = Quaternion.Euler(pos.x/2 , 0, 0);
+        */
 
-        //BlueNinjaからデータ取得
-        BlueComm bc = GetComponent<BlueComm>();
-
-        /*/加速度による移動
-        var dir = Vector3.zero;
-        dir = bc.Acceleration;
+        //加速度による移動
+        /*
+        var dir = bc.Acceleration;
         if(dir != null)
         {
             dir.Normalize();
             transform.Translate(dir);
         }
+        */
 
         //ジャイロによる回転
-        Quaternion gyro = Quaternion.Euler(bc.Gyro);
+        Quaternion gyro = Quaternion.Euler(bc.Gyro); //Gyroはz,x,yの順
         if(gyro != null)
-            transform.rotation = gyro;*/
+            transform.rotation *= gyro; //gyroの値分回転させる
         
     }
 
-    public void OnNavigationStarted(NavigationEventData eventData)
-    {
-        x = eventData.CumulativeDelta.x * 90;
-        y = eventData.CumulativeDelta.y * 90;
-        z = eventData.CumulativeDelta.z * 90;
-    }
-
-    public void OnNavigationUpdated(NavigationEventData eventData)
-    {
-        x = eventData.CumulativeDelta.x * 90;
-        y = eventData.CumulativeDelta.y * 90;
-        z = eventData.CumulativeDelta.z * 90;
-        Debug.Log("x:" + x + " y:" + y + " z:" + z);
-    }
-
-    public void OnNavigationCompleted(NavigationEventData eventData)
-    {
-
-    }
-
-    public void OnNavigationCanceled(NavigationEventData eventData)
-    {
-
-    }
 }
---
+++ BaseballModel/Assets/Scripts/baseball/BatInitBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatInitBehaviour : MonoBehaviour {
+    private GameObject initBat = null;
+    public GameObject batPrefab;
+    public void CreateInitializer()
+    {
+        if (!initBat) //初期化中じゃなければ初期化用ホログラムが存在しない
+        {
+            //既存のバットを消去
+            var oldBat = GameObject.FindWithTag("Bat");
+            if (oldBat)
+                Destroy(oldBat);
+
+            //初期化用ホログラム生成
+            //TODO プリミティブなオブジェクトではなく、Prefabによる初期化用のバットにするべき
+            initBat = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            initBat.transform.position = Camera.main.transform.position + Camera.main.transform.forward.normalized * 0.5f;
+            initBat.transform.localScale = new Vector3(0.067f, 0.83f, 0.067f); 
+        }
+    }
+
+    public void FinishInitializing()
+    {
+        if (initBat)    //初期化中なら初期化用ホログラムが存在する
+        {
+            var newBat = Instantiate(batPrefab);
+            newBat.transform.position = initBat.transform.position;
+
+            Debug.Log(newBat.transform.position +"::"+ initBat.transform.position);
+
+            Destroy(initBat);
+            initBat = null;
+        }
+    }
+}
