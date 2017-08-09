8ebcd5979ce1b8c026d8945f7c9059aeba79c783
norihirosunada <norisuna1201@gmail.com>
2017-08-09 16:27:52 +0900

    ストライクゾーンとかの高さが任意のゲームオブジェクトの高さに初期化される

---
+++ BaseballModel/Assets/Scripts/baseball/init.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init : MonoBehaviour {
+
+    public GameObject floor;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pos = transform.position;
+        pos.y = floor.transform.position.y;
+        transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
