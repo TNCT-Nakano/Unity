6d1afc799a5f782d9fa89115041b66539c9671b3
getpa <catsinf@gmail.com>
2017-09-25 15:43:07 +0900

    no message

--- BaseballModel/Assets/Scripts/baseball/BatBehaviourScript.cs
+++ BaseballModel/Assets/Scripts/baseball/BatBehaviourScript.cs
@@ -18,8 +18,6 @@ public class BatBehaviourScript : MonoBehaviour{
     private Rigidbody rb;
 	private float frame;
 
-    int tmp = 0;
-
     // Use this for initialization
     void Start () {
         //rigidbody.centerOfMass = new Vector3(0, 0, 1);
@@ -33,38 +31,28 @@ public class BatBehaviourScript : MonoBehaviour{
 	// Update is called once per frame
     //60fps
 	void Update () {
-<<<<<<< HEAD
         /*********
          * げっぱ※
          * Unityはフレームの更新速度が可変であるため、frameによる時間の算出は不適切かと
          * Timeクラスにdeltatimeパラメータがあり、前のフレームからの経過時間が取得できるためこちらを用いるとよい
          */
-		float frame = Time.deltaTime;
 
-=======
         //だいたい0.2(1/60)が出る
 		frame = Time.deltaTime;
->>>>>>> 1fb94979eda0240c11bc1f4182df8e59b286becd
-        /*
-        //マウスの位置によって回転させる
-        Vector3 pos = Input.mousePosition;
-        transform.rotation = Quaternion.Euler(pos.x/2 , 0, 0);
-        */
-        /*
-         * バットのIsKinematicをオフにしました。
-         */
+
         //加速度による移動
         acc = SBehav.Accel;
 
         //スレッショルド補正するつもり
 
         //ローカルに力を加える
-<<<<<<< HEAD
-        rb.AddRelativeForce(acc, ForceMode.Acceleration);
+        //rb.AddRelativeForce(acc, ForceMode.Acceleration);
+
+        transform.Translate(acc * frame * frame, Space.Self);
 
         //ジャイロによる回転
         //        Quaternion gyro = Quaternion.Euler(SBehav.Gyro);
-        Vector3 gyro = SBehav.Gyro;
+        gyro = SBehav.Gyro;
         //スレッショルド補正するつもり
 
         //gyroの値分回転させる
@@ -73,24 +61,16 @@ public class BatBehaviourScript : MonoBehaviour{
          * rigidbodyのオブジェクトにはtransformの操作が利きません
          * rigidbodyのプロパティに回転をかけるか、AddRelativeTorqueの利用を検討してください
          */
-        /* スナダ rigidbodyにインスタント速度変化を追加 */
-        rb.AddRelativeTorque(gyro, ForceMode.VelocityChange);
 
-        Debug.Log(acc.ToString() + "," + gyro.ToString());
-=======
-        rb.AddRelativeForce(acc*frame*frame, ForceMode.Acceleration);
-
-        //ジャイロによる回転
-        //        Quaternion gyro = Quaternion.Euler(SBehav.Gyro);
-        gyro = SBehav.Gyro;
-        
         //スレッショルド補正するつもり
 
         //gyroの値分回転させる
         // rigidbodyにインスタント速度変化を追加
-        rb.AddRelativeTorque (gyro, ForceMode.VelocityChange);
-        
->>>>>>> 1fb94979eda0240c11bc1f4182df8e59b286becd
+        //rb.AddRelativeTorque (gyro, ForceMode.VelocityChange);
+
+        transform.Rotate(gyro * frame, Space.Self);
+
+        Debug.Log(acc.ToString() + "," + gyro.ToString());
     }
 
 
